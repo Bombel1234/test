514fd18a26a1fb476fca7b6250bfa7ad493bf3a6
Bombel1234 <129488313+Bombel1234@users.noreply.github.com>
2023-05-23 12:05:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+let a = document.getElementById('entry').value;
+const dialog = document.getElementById("myDialog");
 
+function showDialog(){
+
+    if (a == '123'){
+        document.location.href = 'home.html';
+    }
+    else{
+        dialog.showModal();
+        dialog.style.display = 'block';
+        dialog.style.margin = 'auto auto'
+    }
+    
+}
+function closeDialog() { 
+    dialog.close();
+    dialog.style.display = 'none'; 
+} 
